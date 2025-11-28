65ee0528067fee4ecc443a9f96f5db5a69025812
Marcell <info@creativecrewstudio.co.uk>
2025-11-28 16:12:24 +0200

    Remove test endpoints to fit Vercel free tier limit (12 functions max)

--- api/hello.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { compose, cors, errorHandler } from './_lib/serverless'
-
-async function handler(req: VercelRequest, res: VercelResponse) {
-  res.json({ message: 'Hello from godlyme backend on Vercel' })
-}
-
-export default compose(cors, errorHandler)(handler)
